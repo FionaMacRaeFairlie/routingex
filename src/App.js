b059b04f92582bcbc0c2996c900e400a8f64998a
FionaMacRaeFairlie <88429943+FionaMacRaeFairlie@users.noreply.github.com>
2024-10-17 18:06:05 +0100

    setting up basic routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import Aboutus from "./components/Aboutus";
+import Contactus from "./components/Contactus";
+import Contents from "./components/Contents";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+ return (
+  <BrowserRouter>
+   <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="about" element= {<Aboutus/>} />
+    <Route path="contactus" element= {<Contactus/>} />
+    <Route path="contents" element= {<Contents/>} />
+   </Routes>
+  </BrowserRouter>
+ );
 }
-
 export default App;
